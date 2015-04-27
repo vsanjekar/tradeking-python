f513a3bf72b2694d09f422ecfdba6e86367ae92b
Vinay S <vinay.sanjekar@gmail.com>
2015-04-27 02:41:28 -0700

    GET and POST to tradeking using rauth

--- tradeking/test-oauth.py
+++ tradeking/test-oauth.py
@@ -1,18 +1,54 @@
 __author__ = 'vsanjekar'
 
 from rauth import OAuth1Service
+import json
 
 service = OAuth1Service(
            name='tradeking',
            consumer_key='',
-           consumer_secret='',
-           request_token_url='https://developers.tradeking.com/oauth/request_token',
-           access_token_url='https://developers.tradeking.com/oauth/access_token',
-           authorize_url='https://developers.tradeking.com/oauth/authorize',
-           base_url='https://api.tradeking.com/v1/')
+           consumer_secret='')
 
-request_token, request_token_secret = service.get_request_token()
-print request_token
-print request_token_secret
+oauth_token = ''
+oauth_token_secret = ''
 
-#authorize_url = service.get_authorize_url(request_token)
+
+oauth_session = service.get_session(token=(oauth_token, oauth_token_secret))
+
+print "All watch lists:"
+url = 'https://api.tradeking.com/v1/watchlists.json'
+r = oauth_session.get(url)
+# print type(r)
+print r
+print r.content
+
+watchlist = 'test'
+print "Create watchlist"
+url = 'https://api.tradeking.com/v1/watchlists.json'
+r = oauth_session.post(url, data={'id': watchlist, 'symbols': 'AAPL'})
+print r
+print r.content
+
+print "Add symbols to watchlist"
+url = 'https://api.tradeking.com/v1/watchlists/'+watchlist+'/symbols.json'
+r = oauth_session.post(url, data={'symbols': 'FB'})
+print r
+print r.content
+
+print "Watchlist contents"
+url = 'https://api.tradeking.com/v1/watchlists/'+watchlist+'.json'
+r = oauth_session.get(url)
+print r
+print r.content
+# print json.dumps(json.loads(r.content), indent=2, sort_keys=True)
+
+print "Delete watchlist"
+url = 'https://api.tradeking.com/v1/watchlists/'+watchlist+'.json'
+r = oauth_session.delete(url)
+print r
+print r.content
+
+print "All watch lists:"
+url = 'https://api.tradeking.com/v1/watchlists.json'
+r = oauth_session.get(url)
+print r
+print r.content
